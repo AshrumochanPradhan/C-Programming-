0cd518b3e444efe8cfed0cafa737df39bc78f612
ASHRUMOCHAN PRADHAN <ashrupradhan9110@gmail.com>
2023-02-13 15:20:58 +0530

    DAY-1

---
+++ 01_addition of two numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b=20,sum;
+	
+	sum=a+b;
+	
+	printf("%d\n",sum);
+	
+	return 0;
+}
---
+++ 02_substraction of two numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b=20,sub;
+	
+	sub=a-b;
+	
+	printf("%d\n",sub);
+	
+	
+	return 0;
+}
